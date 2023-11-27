cb53a1846ca91ad67c382973bb00c2c27fcb6889
Tanner <tmmcdaniel909@gmail.com>
2023-11-27 15:42:46 -0500

    first draft of login screen

--- fastapi-frontend/src/pages/index.js
+++ fastapi-frontend/src/pages/index.js
@@ -35,17 +35,59 @@ export default function Home() {
   return (
     <>
       <main className={`${styles.main}`}>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
+                    <h1>Tiny Trackers</h1>
+                    <p>Need help keeping track of medications and logging symptoms for your children? Look no further than TinyTrackers!</p>
+                </div>
+                <div className='col-6'>
+                  {/* log in container */}
+                  <div className='container d-flex justify-content-center align-items-center min-vh-100'> 
+                    <div className='row border rounded-5 p-3 bg-white shadow box-area'>
+                      <div className='col-md-6 rounded-4 justify-content-center align-items-center flex-column left-box'>
 
+                      </div>
+                      <div className='col-md-6 right-box'>
+                        <div className='row align-items-center'>
+                          <div className='header-text mb-4'>
+                            <p>Hello!</p>
+                            <p>Welcome to TinyTrackers!</p>
+                          </div>
+                          <div className='input-group mb-3'>
+                            <input type="text" className='form-control form control-lg bg-light fs-6' placeholder='Email Address'></input>
+                          </div>
+                          <div className='input-group mb-1'>
+                            <input type="password" className='form-control form control-lg bg-light fs-6' placeholder='Password'></input>
+                          </div>
+                          <div className='input-group mb-3 d-flex justify-content-between'>
+                            <div className='forgot mt-2 fs-6'>
+                              <a href='#'>Forgot Password?</a>
+                            </div>
+                            <div className='login-button'>
+                              <button className='btn btn-lg btn-primary w-30 fs-6'>Log In</button>
+                            </div>
+                          </div>
+                          <div className='row mb-3'>
+                            <small>Don't have account? <a href='#'>Sign up</a></small>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
         <div className={styles.grid}>
-        {state.user ? (
-            <li className="nav-item">
-              <Link href="/" className={styles.logout} onClick={handleLogout}>Logout</Link>
-            </li>
-          ) : (
-            <li className="nav-item">
-              <Link href="/login">Login</Link>
-            </li>
-          )}
+          {state.user ? (
+              <li className="nav-item">
+                <Link href="/" className={styles.logout} onClick={handleLogout}>Logout</Link>
+              </li>
+            ) : (
+              <li className="nav-item">
+                <Link href="/login">Login</Link>
+              </li>
+            )}
         </div>
       </main>
     </>
