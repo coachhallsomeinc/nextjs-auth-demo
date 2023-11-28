ed1dbd2e6cb1ceebd36b25ab6b379e55f0d650d1
Tanner <tmmcdaniel909@gmail.com>
2023-11-28 11:49:51 -0500

    navbar, login, and basic landing page

--- fastapi-frontend/src/pages/index.js
+++ fastapi-frontend/src/pages/index.js
@@ -1,4 +1,4 @@
-
+'use client'
 import React, { useState, useEffect } from 'react'
 import { useGlobalState } from '../context/GlobalState';
 import { useRouter } from 'next/navigation';
@@ -6,6 +6,9 @@ import authService from '../services/auth.service';
 import { jwtDecode } from "jwt-decode";
 import styles from '../styles/home.module.css';
 import Link from 'next/link';
+import Navbar from '@/components/navbar';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 export default function Home() {
 
@@ -33,51 +36,51 @@ export default function Home() {
   };
 
   return (
-    <>
-      <main className={`${styles.main}`}>
-        <div className="container">
-            <div className="row">
-                <div className="col-6">
-                    <h1>Tiny Trackers</h1>
-                    <p>Need help keeping track of medications and logging symptoms for your children? Look no further than TinyTrackers!</p>
-                </div>
-                <div className='col-6'>
-                  {/* log in container */}
-                  <div className='container d-flex justify-content-center align-items-center min-vh-100'> 
-                    <div className='row border rounded-5 p-3 bg-white shadow box-area'>
-                      <div className='col-md-6 rounded-4 justify-content-center align-items-center flex-column left-box'>
-
+  <>
+    <div className="container mt-5">
+        <div className='row'>
+          <Navbar />
+        <div className="row mt-5">
+            <div className="col-md-6 border rounded-5 shadow">
+                <h1>Tiny Trackers</h1>
+                <p>Need help keeping track of medications and logging symptoms for your children? Look no further than TinyTrackers!</p>
+            </div>
+            <div className='col-md-6'>
+              {/* log in container */}
+              <div className='container d-flex justify-content-center min-vh-50'> 
+                <div className='row border rounded-5 p-3 bg-white shadow box-area'>
+                  <div className='col-md-12 box'>
+                    <div className='row align-items-center'>
+                      <div className='header-text mb-4'>
+                        <p>Hello!</p>
+                        <p>Welcome to TinyTrackers!</p>
                       </div>
-                      <div className='col-md-6 right-box'>
-                        <div className='row align-items-center'>
-                          <div className='header-text mb-4'>
-                            <p>Hello!</p>
-                            <p>Welcome to TinyTrackers!</p>
-                          </div>
-                          <div className='input-group mb-3'>
-                            <input type="text" className='form-control form control-lg bg-light fs-6' placeholder='Email Address'></input>
-                          </div>
-                          <div className='input-group mb-1'>
-                            <input type="password" className='form-control form control-lg bg-light fs-6' placeholder='Password'></input>
-                          </div>
-                          <div className='input-group mb-3 d-flex justify-content-between'>
-                            <div className='forgot mt-2 fs-6'>
-                              <a href='#'>Forgot Password?</a>
-                            </div>
-                            <div className='login-button'>
-                              <button className='btn btn-lg btn-primary w-30 fs-6'>Log In</button>
-                            </div>
-                          </div>
-                          <div className='row mb-3'>
-                            <small>Don't have account? <a href='#'>Sign up</a></small>
-                          </div>
+                      <div className='input-group mb-3'>
+                        <input type="text" className='form-control form control-lg bg-light fs-6' placeholder='Email Address'></input>
+                      </div>
+                      <div className='input-group mb-1'>
+                        <input type="password" className='form-control form control-lg bg-light fs-6' placeholder='Password'></input>
+                      </div>
+                      <div className='input-group mb-3 d-flex justify-content-between'>
+                        <div className='forgot mt-2 fs-6'>
+                          <a href='#'>Forgot Password?</a>
                         </div>
+                        <div className='login-button'>
+                          <button className='btn btn-lg btn-primary w-30 fs-6'>Log In</button>
+                        </div>
+                      </div>
+                      <div className='row mb-3'>
+                        <small>Don't have account? <a href='#'>Sign up</a></small>
                       </div>
                     </div>
                   </div>
                 </div>
+              </div>
             </div>
         </div>
+      </div>
+    </div>
+      <main className={`${styles.main}`}>
         <div className={styles.grid}>
           {state.user ? (
               <li className="nav-item">
@@ -90,6 +93,6 @@ export default function Home() {
             )}
         </div>
       </main>
-    </>
+  </>
   )
 }
