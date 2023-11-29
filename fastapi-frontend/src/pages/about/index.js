958992a5f731c48f05783311603ab48e37747aa7
Tanner <tmmcdaniel909@gmail.com>
2023-11-29 08:56:03 -0500

    added hotlines to ar page

--- fastapi-frontend/src/pages/about/index.js
+++ fastapi-frontend/src/pages/about/index.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import Navbar from '@/components/navbar';
+import Image from 'next/image';
 
 function about_us() {
 
   return (
     <>
-        <Navbar />
-        <h1 className='mt-5'>About Us</h1>
-        <p>Blah blah blah about us stuff here</p>
+    <Navbar />
+    <div className='container mt-5'>
+      <div className='row'>
+        <div className='col-6'>
+          <h1>About Us</h1>
+          <p>Blah blah blah about us stuff here</p>
+        </div>
+        <div className='col-6'>
+          <Image className='rounded-5 mt-5' src="/img/baby-at-doctor.jpg" alt="baby at doctor" width={500} height={300}/>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
--- fastapi-frontend/src/pages/additional-resources/index.js
+++ fastapi-frontend/src/pages/additional-resources/index.js
@@ -5,8 +5,24 @@ function additionalResources() {
   return (
     <>
         <Navbar/>
-        <h1 className='mt-5'>Additional Resources</h1>
-        <p>Blahblahblah</p>
+        <div className='conatiner'>
+          <div className='row'>
+            <div className='col-6 ms-4'>
+              <h1 className='mt-5'>Additional Resources</h1>
+              <ul>
+                <li className='mt-2'>Poison Control Center: Hotline: 1-800-222-1222 (USA)</li>
+                <li className='mt-2'>National Suicide Prevention Lifeline: Hotline 1-800-273-TALK (1-800-273-8255) (USA)</li>
+                <li className='mt-2'>Childhelp National Child Abuse Hotline: Hotline 1-800-4-A-CHILD (1-800-422-4453) (USA)</li>
+                <li className='mt-2'>National Domestic Violence Hotline: Hotline 1-800-799-SAFE (1-800-799-7233) (USA)</li>
+                <li className='mt-2'>Substance Abuse and Mental Health Services Administration (SAMHSA) National Helpline: Hotline 1-800-662-HELP (1-800-662-4357) (USA)</li>
+                <li className='mt-2'>National Eating Disorders Association (NEDA) Helpline: Hotline 1-800-931-2237 (USA)</li>
+                <li className='mt-2'>National Alliance on Mental Illness (NAMI) Helpline: Helpline 1-800-950-NAMI (1-800-950-6264) (USA)</li>
+                <li className='mt-2'>National Runaway Safeline: Hotline 1-800-RUNAWAY (1-800-786-2929) (USA)</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        
     </>
   )
 }
