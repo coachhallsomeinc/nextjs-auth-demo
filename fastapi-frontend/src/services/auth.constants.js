b806775cf131417f133c8bc9b433364f51c898a4
Coach Hall <51966143+coachhallsomeinc@users.noreply.github.com>
2023-12-04 16:34:10 -0500

    Update auth.constants.js

--- fastapi-frontend/src/services/auth.constants.js
+++ fastapi-frontend/src/services/auth.constants.js
@@ -2,3 +2,5 @@ export const API_URL = "https://8000-coachhallso-fastapiauth-ncq61p1ghmk.ws-us10
 export const LOGIN_ENDPOINT = "login/access-token"
 export const REGISTER_ENDPOINT = "users/register/"
 export const REFRESH_ENDPOINT = "token/refresh/"
+export const GET_DATA_ENDPOINT = "data/"
+export const SEND_DATA_ENDPOINT = "data/"
