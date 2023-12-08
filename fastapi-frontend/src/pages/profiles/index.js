0a6b36fa7f56a5c6ad532afeaff6749c353f612b
Tanner <tmmcdaniel909@gmail.com>
2023-12-08 18:34:29 -0500

    done for the day

--- fastapi-frontend/src/pages/profiles/index.js
+++ fastapi-frontend/src/pages/profiles/index.js
@@ -56,22 +56,21 @@ function ProfilesPage() {
   const data = {
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
-      "Authorization": "Bearer " + state.user.access_token,
-    }
-  }
+      Authorization: "Bearer " + state.user.access_token,
+    },
+  };
 
-  useEffect(async () => {
+  useEffect(() => {
     const data = {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": "Bearer " + state.user.access_token,
-      }
-    }
-    const childData = await ChildService.getChildData(data)
-    console.log(childData)
-    setChildToRender(childData)
-  },[])
-
+        Authorization: "Bearer " + state.user.access_token,
+      },
+    };
+    ChildService.getChildData(data).then((result) => {
+      setChildToRender(result);
+    });
+  }, []);
 
   // console.log(childData)
 
@@ -82,15 +81,37 @@ function ProfilesPage() {
           <div className="row">
             <Navbar />
           </div>
-          <div className="row">
-            <div className="col-4"></div>
-            <div className="col-4 py-3 mt-5 text-center border rounded-5 shadow" id={styles.componentcolor}>
-              <h3 id={styles.textbasefont}>View your family profiles</h3>
-            </div>
-          </div>
+
           <div className="row mt-1">
             <div className="col-2"></div>
             <div className="col-8">
+              <h1 className="mt-5" id={styles.textbasefont}>
+                Family Profiles
+              </h1>
+              <div
+                className="col py-3 mt-5 text-center border rounded-5 shadow"
+                id={styles.componentcolor}
+              >
+                <h5 id={styles.textbasefont}>
+                  Want to add a new child to your family?
+                </h5>
+                <Link
+                  id={styles.textbasefont}
+                  style={{ fontSize: "large" }}
+                  className="btn btn-lg btn-primary w-30 fs-6"
+                  href="/child-code-register"
+                >
+                  Enter Child ID Code
+                </Link>
+                <Link
+                  id={styles.textbasefont}
+                  style={{ fontSize: "large" }}
+                  className="mx-3 btn btn-lg btn-primary w-30 fs-6"
+                  href="/child-register-form"
+                >
+                  Register new Child
+                </Link>
+              </div>
               <div>
                 <div className="accordion mt-5 shadow rounded-5">
                   <div className="accordion-item" id={styles.componentcolor}>
@@ -121,59 +142,63 @@ function ProfilesPage() {
                       </div>
                     </div>
                   </div>
-                  {childToRender.map((child) => 
-                  {return (
-                  <div key={child.unique_child_code} className="accordion-item" id={styles.componentcolor}>
-                    <h2 className="accordion-header">
-                      <button
+                  {childToRender.map((child) => {
+                    return (
+                      <div
+                        key={child.unique_child_code}
+                        className="accordion-item"
                         id={styles.componentcolor}
-                        className="accordion-button collapsed"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target={`#panelsStayOpen-collapse${child.unique_child_code}`}
-                        aria-expanded="false"
-                        aria-controls={`panelsStayOpen-collapse${child.unique_child_code}`}
                       >
-                        <div id={styles.textbasefont}>
-                          {child.first_name}
+                        <h2 className="accordion-header">
+                          <button
+                            id={styles.componentcolor}
+                            className="accordion-button collapsed"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target={`#panelsStayOpen-collapse${child.unique_child_code}`}
+                            aria-expanded="false"
+                            aria-controls={`panelsStayOpen-collapse${child.unique_child_code}`}
+                          >
+                            <div id={styles.textbasefont}>
+                              {child.first_name}
+                            </div>
+                          </button>
+                        </h2>
+                        <div
+                          id={`panelsStayOpen-collapse${child.unique_child_code}`}
+                          className="accordion-collapse collapse"
+                        >
+                          <div
+                            id={styles.textbasefont}
+                            className="accordion-body"
+                          >
+                            <ul>
+                              <li>First Name: {child.first_name}</li>
+                              <li>Last Name: {child.last_name}</li>
+                              <li>
+                                Unique Child Code: {child.unique_child_code}
+                              </li>
+                              <li>Date of Birth: {child.dob}</li>
+                              <li>Allergies: {child.allergies}</li>
+                              <li>
+                                Pediatrician Name: {child.pediatrician_name}
+                              </li>
+                              <li>
+                                Pediatrician Phone Number:{" "}
+                                {child.pediatrician_number}
+                              </li>
+                            </ul>
+                          </div>
                         </div>
-                      </button>
-                    </h2>
-                    <div
-                      id={`panelsStayOpen-collapse${child.unique_child_code}`}
-                      className="accordion-collapse collapse"
-                    >
-                      <div id={styles.textbasefont} className="accordion-body">
-                        <ul>
-                          <li>First Name: {child.first_name}</li>
-                          <li>Last Name: {child.last_name}</li>
-                          <li>Unique Child Code: {child.unique_child_code}</li>
-                          <li>Date of Birth: {child.dob}</li>
-                          <li>Allergies: {child.allergies}</li>
-                          <li>Pediatrician Name: {child.pediatrician_name}</li>
-                          <li>Pediatrician Phone Number: {child.pediatrician_number}</li>
-                        </ul>
                       </div>
-                    </div>
-                  </div>)}
-                  )}
+                    );
+                  })}
                 </div>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col-4"></div>
-            <div className="col-4 py-3 mt-5 text-center border rounded-5 shadow" id={styles.componentcolor}>
-              <h5 id={styles.textbasefont}>Want to add a new child to your family?</h5>
-              <Link
-                  id={styles.textbasefont}
-                  style={{ fontSize: "large" }}
-                  className="btn btn-lg btn-primary w-30 fs-6"
-                  href="/child-code-register"
-              >
-                  Enter Child ID Code
-              </Link>       
-            </div>
           </div>
         </div>
       </div>
